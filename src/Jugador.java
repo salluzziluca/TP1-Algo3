351766e8d46531fb554db17e89d58083dbc974f7
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2022-10-25 20:48:20 -0300

    feat: Asignarle vida al jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -2,10 +2,11 @@ package src;
 
 public class Jugador {
     private String nombre;
+    private int vida;
     private Mano mano;
     private Mazo mazo;
 
-    public Jugador(String nombre, Mano mano, Mazo mazo) {
+    public Jugador(String nombre, int vida, Mano mano, Mazo mazo) {
         this.nombre = nombre;
         this.mano = mano;
         this.mazo = mazo;
--- src/JugadorTest.java
+++ src/JugadorTest.java
@@ -10,7 +10,7 @@ public class JugadorTest {
         Carta carta = new CartaDeAtaque("Carta de prueba", "Esta es una carta de prueba", 1, 1);
         Mazo mazo = new Mazo();
         mazo.agregarCarta(carta);
-        Jugador jugador = new Jugador("Jugador de prueba", new Mano(), mazo);
+        Jugador jugador = new Jugador("Jugador de prueba", 7, new Mano(), mazo);
         jugador.robarCarta();
         assertEquals(jugador.getCantidadCartasEnMano(), 1);
     }
