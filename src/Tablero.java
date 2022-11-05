77f082406db6a998b8d5b625de68a197d9624b6a
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2022-11-05 16:48:42 -0300

    feat: empezar a desarollar los secretos
    se creo un observer con el tablero como EventManager
    y los jugadores como observers

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,20 +1,32 @@
 package src;
 
 public class Tablero {
-    public Jugador jugador1;
-    public Jugador jugador2;
-    public int turno;
+    private Jugador jugador1;
+    private Jugador jugador2;
+    private Carta cartaEnJuego;
+    private int turno;
 
     public Tablero(Jugador jugador1, Jugador jugador2) {
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
+        this.cartaEnJuego = null;
+        this.turno = 0;
     }
 
     public void jugar() {
         while (jugador1.estaVivo() && jugador2.estaVivo()) {
-            jugador1.jugarTurno(jugador2);
-            jugador2.jugarTurno(jugador1);
+            jugador1.jugarTurno(jugador2, this);
+            jugador2.jugarTurno(jugador1, this);
             turno++;
         }
     }
+
+    public void setCartaEnJuego(Carta carta) {
+        this.cartaEnJuego = carta;
+    }
+
+    public void notificar() {
+        jugador1.update(cartaEnJuego);
+     
+    } 
 }
