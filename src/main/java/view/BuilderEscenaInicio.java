6e8e5d00ebe83699e69d3cfd807f0804ec7c6d25
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2022-11-29 15:50:19 -0300

    style: condensar los imports

--- src/main/java/view/BuilderEscenaInicio.java
+++ src/main/java/view/BuilderEscenaInicio.java
@@ -3,13 +3,8 @@ package view;
 import controller.ObserverRecibirNombreYMazo;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 
 public class BuilderEscenaInicio {
 
